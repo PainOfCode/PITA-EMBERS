4253c5816145726b97a62c0573b721511656ac54
PainOfCode <m.merz874@gmail.com>
2019-01-29 07:15:38 +0100

    CSS rework

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,8 +7,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  app.import('bower_component/moment/min/moment.min.js')
-  // Use `app.import` to add additional libraries to the generated
+   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
   // If you need to use different assets in different
@@ -21,7 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('app/styles/app.css');
 
   return app.toTree();
 };
